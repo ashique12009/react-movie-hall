4fbde747a4873774f1ce5a81675f05aa964e09e5
ashique <ashique12009@gmail.com>
2024-06-04 00:12:52 +0300

    data fetching from movie server

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import MovieCard from './components/MovieCard'
 import SearchBar from './components/SearchBar'
 
 function App() {
+
+  const [allMovieData, setAllMovieData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchMovieData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(`https://www.omdbapi.com/?s=${search}&apikey=6852330c`);
+      const data = await response.json();
+      setAllMovieData(data.Search);
+      setLoading(false);
+    } 
+    catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
   return (
     <div>
       <Navbar />
-      <SearchBar />
-      <MovieCard />
+      <div className='bg'>
+        <SearchBar searchMovie={search} setSearchMovie={setSearch} fetchMovieData={fetchMovieData} />
+        <MovieCard allMovieData={allMovieData} loading={loading} />
+      </div>
     </div>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
-const SearchBar = () => {
+const SearchBar = ({searchMovie, setSearchMovie, fetchMovieData}) => {
   return (
     <div className='main flex justify-center py-5'>
         <input 
             type="text" 
             placeholder='Search' 
+            value={searchMovie}
+            onChange={(e) => {setSearchMovie(e.target.value)}}
             className='bg-grey-200 placeholder-grey-400 px-2 py-2 outline-none border-2 border-grey-500 text-black rounded-l-lg' 
         />
-        <button className='bg-[#3e2b57] text-white px-2 py-2 rounded-r-lg'>
+        <button 
+            onClick={fetchMovieData}
+            className='bg-[#3e2b57] text-white px-2 py-2 rounded-r-lg'>
             Search
         </button>
     </div>
