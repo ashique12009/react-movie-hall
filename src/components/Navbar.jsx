e2b3a37d71be584ac29f99e8143627291b887af7
ashique <ashique12009@gmail.com>
2024-06-03 16:31:50 +0300

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Logo from '../assets/logo.jpg'
 
 const Navbar = () => {
   return (
-    <div className='main flex justify-between items-center px-4 py-4 bg-[#3e2b57]'>
+    <div className='main lg:flex md:flex flex-wrap justify-between items-center px-4 py-4 bg-[#3e2b57]'>
         <div className='left flex items-center space-x-3'>
             <img src={Logo} alt="logo" />
             <h2 className='text-white text-2xl font-bold'>MovieHall</h2>
