8d6c737e3691d6727f3dc379f1b940617dab0f28
ashique <ashique12009@gmail.com>
2024-06-03 16:59:51 +0300

    movie card design done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,49 @@ import React from 'react'
 
 const MovieCard = () => {
   return (
-    <div>MovieCard</div>
+    <div>
+      <div className='main flex flex-wrap'>
+        <div className='p-2 child lg:w-1/4 w-full'>
+          <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
+            <img src="https://dummyimage.com/400x400" alt="img" className='rounded-lg mb-2 w-full' />
+            <h2 className='text-white text-xl font-bold'>Title</h2>
+            <h2 className='text-white'>Desc</h2>
+          </div>
+        </div>
+
+        <div className='p-2 child lg:w-1/4 w-full'>
+          <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
+            <img src="https://dummyimage.com/400x400" alt="img" className='rounded-lg mb-2 w-full' />
+            <h2 className='text-white text-xl font-bold'>Title</h2>
+            <h2 className='text-white'>Desc</h2>
+          </div>
+        </div>
+
+        <div className='p-2 child lg:w-1/4 w-full'>
+          <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
+            <img src="https://dummyimage.com/400x400" alt="img" className='rounded-lg mb-2 w-full' />
+            <h2 className='text-white text-xl font-bold'>Title</h2>
+            <h2 className='text-white'>Desc</h2>
+          </div>
+        </div>
+
+        <div className='p-2 child lg:w-1/4 w-full'>
+          <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
+            <img src="https://dummyimage.com/400x400" alt="img" className='rounded-lg mb-2 w-full' />
+            <h2 className='text-white text-xl font-bold'>Title</h2>
+            <h2 className='text-white'>Desc</h2>
+          </div>
+        </div>
+
+        <div className='p-2 child lg:w-1/4 w-full'>
+          <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
+            <img src="https://dummyimage.com/400x400" alt="img" className='rounded-lg mb-2 w-full' />
+            <h2 className='text-white text-xl font-bold'>Title</h2>
+            <h2 className='text-white'>Desc</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
