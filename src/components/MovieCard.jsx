ae3c1460c8c43b8af81ffe44cdb7cc1f4b5b39b7
ashique <ashique12009@gmail.com>
2024-06-04 00:19:22 +0300

    movie items are showing in movie card component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,48 +1,24 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = ({allMovieData, loading}) => {
   return (
     <div>
       <div className='main flex flex-wrap'>
-        <div className='p-2 child lg:w-1/4 w-full'>
-          <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
-            <img src="https://dummyimage.com/400x400" alt="img" className='rounded-lg mb-2 w-full' />
-            <h2 className='text-white text-xl font-bold'>Title</h2>
-            <h2 className='text-white'>Desc</h2>
-          </div>
-        </div>
 
-        <div className='p-2 child lg:w-1/4 w-full'>
-          <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
-            <img src="https://dummyimage.com/400x400" alt="img" className='rounded-lg mb-2 w-full' />
-            <h2 className='text-white text-xl font-bold'>Title</h2>
-            <h2 className='text-white'>Desc</h2>
-          </div>
-        </div>
-
-        <div className='p-2 child lg:w-1/4 w-full'>
-          <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
-            <img src="https://dummyimage.com/400x400" alt="img" className='rounded-lg mb-2 w-full' />
-            <h2 className='text-white text-xl font-bold'>Title</h2>
-            <h2 className='text-white'>Desc</h2>
-          </div>
-        </div>
-
-        <div className='p-2 child lg:w-1/4 w-full'>
-          <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
-            <img src="https://dummyimage.com/400x400" alt="img" className='rounded-lg mb-2 w-full' />
-            <h2 className='text-white text-xl font-bold'>Title</h2>
-            <h2 className='text-white'>Desc</h2>
-          </div>
-        </div>
-
-        <div className='p-2 child lg:w-1/4 w-full'>
-          <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
-            <img src="https://dummyimage.com/400x400" alt="img" className='rounded-lg mb-2 w-full' />
-            <h2 className='text-white text-xl font-bold'>Title</h2>
-            <h2 className='text-white'>Desc</h2>
-          </div>
-        </div>
+        {
+          allMovieData.map((item, index) => {
+            return (
+              <div className='p-2 child lg:w-1/4 w-full'>
+                <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
+                  <img src={item.Poster} alt="img" className='rounded-lg mb-2 w-full' />
+                  <h2 className='text-white text-xl font-bold'>{item.Title}</h2>
+                  <h2 className='text-white'>Year: {item.Year}</h2>
+                </div>
+              </div>
+            )
+          })
+        }
+        
       </div>
     </div>
   )
