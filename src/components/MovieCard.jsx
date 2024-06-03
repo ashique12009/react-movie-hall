46cba5e8be8c1b17c49df4d3ab5e5e7855febad9
ashique <ashique12009@gmail.com>
2024-06-04 00:31:27 +0300

    project done for the first phase, as movie searching feature is working, showing all movies in movie card one by one as item, during fetching a loader is implemented

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,12 +3,17 @@ import React from 'react'
 const MovieCard = ({allMovieData, loading}) => {
   return (
     <div>
-      <div className='main flex flex-wrap'>
 
+      {
+        loading 
+        ? 
+          <div className='flex justify-center'><img className='w-16 py-20' src="https://i.gifer.com/ZZ5H.gif" /></div>
+        : 
+        <div className='main flex flex-wrap'>
         {
           allMovieData.map((item, index) => {
             return (
-              <div className='p-2 child lg:w-1/4 w-full'>
+              <div key={index} className='p-2 child lg:w-1/4 w-full'>
                 <div className='sub_child bg-[#002e4b] p-3 rounded-2xl'>
                   <img src={item.Poster} alt="img" className='rounded-lg mb-2 w-full' />
                   <h2 className='text-white text-xl font-bold'>{item.Title}</h2>
@@ -18,8 +23,9 @@ const MovieCard = ({allMovieData, loading}) => {
             )
           })
         }
+        </div>
+      }
         
-      </div>
     </div>
   )
 }
